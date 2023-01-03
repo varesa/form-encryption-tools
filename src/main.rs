75f33b871ccc38daacec15fb6bf055fd119e57d5
Esa Varemo <git@esav.fi>
2023-01-03 21:41:17 +0200

    Refactoring and clippy

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,21 @@ use anyhow::Context;
 use clap::Parser;
 use log::info;
 use notify::event::AccessKind;
-use notify::{EventKind, Watcher};
+use notify::{Event, EventKind, Watcher};
 use openssl::rsa::Padding;
-use serde_derive::Serialize;
-use std::ffi::OsStr;
-use std::fs::File;
-use std::io::ErrorKind::NotFound;
-use std::path::PathBuf;
-use std::{fs, sync};
+use std::path::{Path, PathBuf};
+use std::fs;
+use std::sync::mpsc::{channel, Receiver};
 
 use crate::config::{Config, ConfigFile, Target};
 use crate::keys::RsaKeyfile;
 use crate::symmetric_cipher::SymmetricCipher;
+use crate::bundle::Bundle;
 
 mod config;
 mod keys;
 mod symmetric_cipher;
+mod bundle;
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -34,9 +33,18 @@ struct Cli {
     output: PathBuf,
 }
 
+fn watch_files(path: &Path) -> Result<Receiver<notify::Result<Event>>, anyhow::Error> {
+    let (tx, rx) = channel();
+
+    let mut watcher = notify::recommended_watcher(tx)?;
+    watcher.watch(path, notify::RecursiveMode::NonRecursive)?;
+    Ok(rx)
+}
+
 fn main() -> Result<(), anyhow::Error> {
     simple_logger::init_with_level(log::Level::Info).unwrap();
     let cli = Cli::parse();
+
     let config_string = fs::read_to_string(&cli.config)
         .context(format!("Error reading the config file: {:?}", &cli.config))?;
     let config_file: ConfigFile = toml::from_str(&config_string)?;
@@ -45,12 +53,7 @@ fn main() -> Result<(), anyhow::Error> {
         output: cli.output,
     };
 
-    let (tx, rx) = sync::mpsc::channel();
-
-    let mut watcher = notify::recommended_watcher(tx)?;
-    watcher.watch(&cli.input, notify::RecursiveMode::NonRecursive)?;
-
-    for event in rx.iter() {
+    for event in watch_files(&cli.input)?.iter() {
         let event = event?;
         if let EventKind::Access(AccessKind::Close(_)) = &event.kind {
             for path in event.paths {
@@ -61,10 +64,19 @@ fn main() -> Result<(), anyhow::Error> {
     Err(anyhow::format_err!("Exited the main loop"))
 }
 
-#[derive(Debug, Serialize)]
-struct Bundle {
-    ciphertext: Vec<u8>,
-    enc_key: Vec<u8>,
+fn handle_file(file: PathBuf, config: &Config) -> Result<(), anyhow::Error> {
+    info!("Handling file: {}", file.display());
+    let filename = file
+        .file_name()
+        .ok_or_else(|| anyhow::Error::msg("Unable to get input filename"))?;
+    let plaintext_content = std::fs::read_to_string(&file)?;
+
+    for target in &config.targets {
+        info!(".. with target {}", &target.name);
+        let bundle = encrypt_for(&plaintext_content, target)?;
+        bundle.write_to_path(&config.output, &target.name, filename)?;
+    }
+    Ok(())
 }
 
 fn encrypt_for(plaintext: &str, target: &Target) -> Result<Bundle, anyhow::Error> {
@@ -72,63 +84,15 @@ fn encrypt_for(plaintext: &str, target: &Target) -> Result<Bundle, anyhow::Error
     let ciphertext = sym_cipher.encrypt(plaintext.as_bytes())?;
 
     let rsa_key = RsaKeyfile::from_url(&target.key_url)?.into_rsa_key()?;
-    let mut enc_sym_key = vec![0; rsa_key.size() as usize];
+    let mut sym_enc_key = vec![0; rsa_key.size() as usize];
     rsa_key.public_encrypt(
         sym_cipher.get_key().as_slice(),
-        enc_sym_key.as_mut_slice(),
+        sym_enc_key.as_mut_slice(),
         Padding::PKCS1,
     )?;
 
     Ok(Bundle {
-        ciphertext: ciphertext.clone(),
-        enc_key: enc_sym_key,
+        ciphertext,
+        enc_key: sym_enc_key,
     })
-}
-
-fn write_bundle(
-    bundle: &Bundle,
-    output_path: &PathBuf,
-    target: &str,
-    filename: &OsStr,
-) -> Result<(), anyhow::Error> {
-    let target_dir = PathBuf::from(output_path).join(&target.name);
-
-    match std::fs::metadata(&target_dir) {
-        Err(e) => {
-            assert_eq!(e.kind(), NotFound);
-            info!(
-                ".. parent {} does not exist, creating",
-                target_dir.display()
-            );
-            std::fs::create_dir(&target_dir)?;
-        }
-        Ok(f) => {
-            assert!(f.is_dir());
-        }
-    };
-
-    let file_path = PathBuf::from(&target_dir).join(filename);
-    //
-    info!(".. output to: {}", &file_path.display());
-
-    info!(".. writing");
-    let file = File::create(out_path)?;
-    bincode::serialize_into(file, &bundle)?;
-
-    Ok(())
-}
-
-fn handle_file(file: PathBuf, config: &Config) -> Result<(), anyhow::Error> {
-    info!("Handling file: {}", file.display());
-    let filename = file
-        .file_name()
-        .ok_or(anyhow::Error::msg("Unable to get input filename"))?;
-    let plaintext_content = std::fs::read_to_string(&file)?;
-
-    for target in &config.targets {
-        info!(".. with target {}", &target.name);
-        let bundle = encrypt_for(&plaintext_content, target)?;
-        write_bundle(&bundle, &config.output, &target.name, filename)
-    }
-    Ok(())
 }
