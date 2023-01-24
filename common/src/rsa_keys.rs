35b8f6860c988a7ae3746b75d85eb4c08ccc9ccd
Esa Varemo <esa@kuivanto.fi>
2023-01-24 02:39:41 +0200

    Add key converter and refactor BASE64URL_NOPAD conversions

--- common/src/rsa_keys.rs
+++ common/src/rsa_keys.rs
@@ -1,9 +1,22 @@
 use anyhow::Context;
+use data_encoding::BASE64URL_NOPAD;
 use log::info;
+use openssl::bn::{BigNum, BigNumRef};
 use openssl::pkey;
 use openssl::rsa::Rsa;
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
 use std::io::Read;
+
+pub fn encode(x: &BigNumRef) -> String {
+    let raw = x.to_vec();
+    BASE64URL_NOPAD.encode(&raw)
+}
+
+pub fn decode(x: &str) -> Result<BigNum, anyhow::Error> {
+    let raw = BASE64URL_NOPAD.decode(x.as_bytes())?;
+    let bn = BigNum::from_slice(&raw)?;
+    Ok(bn)
+}
 
 pub trait KeyFromString<T> {
     fn from_raw_string(data: &str) -> Result<T, anyhow::Error>;
@@ -26,7 +39,7 @@ where
     }
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct RsaPubkey {
     kty: String,
     n: String,
@@ -34,6 +47,10 @@ pub struct RsaPubkey {
 }
 
 impl RsaPubkey {
+    pub fn from_parts(kty: String, n: String, e: String) -> Self {
+        Self { kty, n, e }
+    }
+
     pub fn into_rsa_key(self) -> Result<Rsa<pkey::Public>, anyhow::Error> {
         if &self.kty != "RSA" {
             return Err(anyhow::format_err!(
@@ -41,18 +58,8 @@ impl RsaPubkey {
                 &self.kty
             ));
         }
-
-        let n_slice = data_encoding::BASE64URL_NOPAD
-            .decode(self.n.as_bytes())
-            .context("Decoding n")?;
-        let e_slice = data_encoding::BASE64URL_NOPAD
-            .decode(self.e.as_bytes())
-            .context("Decoding e")?;
-        let rsa_key = openssl::rsa::Rsa::from_public_components(
-            openssl::bn::BigNum::from_slice(&n_slice)?,
-            openssl::bn::BigNum::from_slice(&e_slice)?,
-        )
-        .context("Building RSA key from components")?;
+        let rsa_key = openssl::rsa::Rsa::from_public_components(decode(&self.n)?, decode(&self.e)?)
+            .context("Building RSA key from components")?;
         Ok(rsa_key)
     }
 }
@@ -85,47 +92,15 @@ impl RsaPrivateKey {
             ));
         }
 
-        let n_slice = data_encoding::BASE64URL_NOPAD
-            .decode(self.n.as_bytes())
-            .context("Decoding n")
-            .unwrap();
-        let e_slice = data_encoding::BASE64URL_NOPAD
-            .decode(self.e.as_bytes())
-            .context("Decoding e")
-            .unwrap();
-        let d_slice = data_encoding::BASE64URL_NOPAD
-            .decode(self.d.as_bytes())
-            .context("Decoding d")
-            .unwrap();
-        let p_slice = data_encoding::BASE64URL_NOPAD
-            .decode(self.p.as_bytes())
-            .context("Decoding p")
-            .unwrap();
-        let q_slice = data_encoding::BASE64URL_NOPAD
-            .decode(self.q.as_bytes())
-            .context("Decoding q")
-            .unwrap();
-        let dmp1_slice = data_encoding::BASE64URL_NOPAD
-            .decode(self.dmp1.as_bytes())
-            .context("Decoding dmp")
-            .unwrap();
-        let dmq1_slice = data_encoding::BASE64URL_NOPAD
-            .decode(self.dmq1.as_bytes())
-            .context("Decoding dmq")
-            .unwrap();
-        let iqmp_slice = data_encoding::BASE64URL_NOPAD
-            .decode(self.iqmp.as_bytes())
-            .context("Decoding iqmp")
-            .unwrap();
         let rsa_key = openssl::rsa::Rsa::from_private_components(
-            openssl::bn::BigNum::from_slice(&n_slice).unwrap(),
-            openssl::bn::BigNum::from_slice(&e_slice).unwrap(),
-            openssl::bn::BigNum::from_slice(&d_slice).unwrap(),
-            openssl::bn::BigNum::from_slice(&p_slice).unwrap(),
-            openssl::bn::BigNum::from_slice(&q_slice).unwrap(),
-            openssl::bn::BigNum::from_slice(&dmp1_slice).unwrap(),
-            openssl::bn::BigNum::from_slice(&dmq1_slice).unwrap(),
-            openssl::bn::BigNum::from_slice(&iqmp_slice).unwrap(),
+            decode(&self.n)?,
+            decode(&self.e)?,
+            decode(&self.d)?,
+            decode(&self.p)?,
+            decode(&self.q)?,
+            decode(&self.dmp1)?,
+            decode(&self.dmq1)?,
+            decode(&self.iqmp)?,
         )
         .context("Building RSA key from components")
         .unwrap();
---
+++ tools/src/bin/convert_key.rs
@@ -0,0 +1,22 @@
+use common::rsa_keys::{encode, RsaPubkey};
+use openssl::rsa::Rsa;
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = args.get(1).expect("File name not provided");
+    dbg!(filename);
+
+    let keydata = fs::read(filename).expect("Unable to read key file");
+    let key = Rsa::private_key_from_pem(&keydata).expect("Unable to understand key");
+
+    let n = encode(key.n());
+    let e = encode(key.e());
+
+    println!(
+        "{}",
+        serde_json::to_string(&RsaPubkey::from_parts("RSA".to_string(), n, e,))
+            .expect("Failed to encode key as JSON")
+    )
+}
